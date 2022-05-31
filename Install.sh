3c8a5efa3092f8ecac100497ec0ea2746a671518
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-05-31 23:26:19 +0200

    [REMOVE] Remove daemon (update installation script and README.md)

--- Install.sh
+++ Install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-cp battery_notifier.service battery_notifier_complete.service
-
-echo "[Install.sh] Percentage when the notification should be sent:"
-read percentage
-sed -i -e "s/PERCENTAGE/$percentage/g" battery_notifier_complete.service
+PATH=/usr/bin
 
 echo "[Install.sh] Build binary ..."
 cargo build --release
-echo "[Install.sh] Copy files to final locations ..."
-sudo cp target/release/battery_notifier /usr/bin/
-sudo cp battery_notifier_complete.service /etc/systemd/system/battery_notifier.service
-echo "[Install.sh] Enable and start daemon ..."
-sudo systemctl enable --now battery_notifier.service
+echo "[Install.sh] Copy files in $PATH"
+sudo cp target/release/battery_notifier $PATH
--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 A small deamon which send a notification if the battery level reach a certain value. (For linux, use libnotify)
 
 ## Usage:
-Run:
+Run the following command to install:
 ```sh
 ./Install.sh
+```
+
+Put the following command in order to execute the program when login on the session:
+```sh
+battery_notifier <PERCENTAGE>
+# <PERCENTAGE> is the percentage of battery when the notification should be sent.
+```
+### Example for i3:
+In i3/config put:
+```sh
+exec --no-startup-id battery_notifier <PERCENTAGE>
 ```
